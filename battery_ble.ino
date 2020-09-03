06fe1971d27a442d034f0d67ee3cfd55dc99fdfc
nicolas-f <[omitted]>
2020-09-03 17:55:12 +0200

    read connected battery and send the level each 5 seconds

---
+++ battery_ble.ino
@@ -0,0 +1,94 @@
+
+
+/*Program to use GATT service on ESP32 to send Battery Level
+ * ESP32 works as server - Mobile as client
+ * Program by: B.Aswinth Raj
+ * Dated on: 13-10-2018
+ * Website: www.circuitdigest.com
+ */
+
+int sensorPin = A13;    // select the input pin for the potentiometer
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h> //Library to use BLE as server
+#include <BLE2902.h> 
+
+bool _BLEClientConnected = false;
+
+#define BatteryService BLEUUID((uint16_t)0x180F) 
+BLECharacteristic BatteryLevelCharacteristic(BLEUUID((uint16_t)0x2A19), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      _BLEClientConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      _BLEClientConnected = false;
+    }
+};
+
+void InitBLE() {
+  BLEDevice::init("BLE Battery");
+
+  // push power
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL0, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL1, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL2, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL3, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL4, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL5, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL6, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL8, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
+
+  
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pBattery = pServer->createService(BatteryService);
+
+  pBattery->addCharacteristic(&BatteryLevelCharacteristic);
+  BatteryLevelDescriptor.setValue("Battery voltage");
+  BatteryLevelCharacteristic.addDescriptor(&BatteryLevelDescriptor);
+  BatteryLevelCharacteristic.addDescriptor(new BLE2902());
+
+  pServer->getAdvertising()->addServiceUUID(BatteryService);
+
+  pBattery->start();
+
+
+  
+  // Start advertising
+  pServer->getAdvertising()->start();
+}
+const float bat_min = 3.00; // volts
+const float bat_max = 4.2;  // volts
+
+
+void setup() {
+  
+  Serial.begin(115200);
+  
+  Serial.println("Battery Level Indicator - BLE");
+  
+  InitBLE();
+
+  while(1) {    
+    float sensorValue = (analogRead(sensorPin) / 4095.0)*2.0*3.3*1.1;
+    uint8_t level = (uint8_t)min(100, max(0, (int)map((int)(sensorValue * 100), (int)(bat_min * 100), (int)(bat_max * 100), 0, 100))); 
+    BatteryLevelCharacteristic.setValue(&level, 1);
+    BatteryLevelCharacteristic.notify();
+    Serial.print("Battery Level ");
+    Serial.print(sensorValue);
+    Serial.print(" - ");
+    Serial.println(level);
+    delay(5000);
+  }
+}
+
+void loop() { }
